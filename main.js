cf75e299b5a202f67c8d3fa4ba81dc812a8eebaf
AllouhM <mahmoud.allouh0@gmail.com>
2019-03-17 21:49:14 +0100

    adding files

---
+++ main.js
@@ -0,0 +1,131 @@
+
+const btn = document.getElementById("btn")
+const nameInput = document.getElementById("input-name")
+const ageInput = document.getElementById("input-age");
+const hobbyInput = document.getElementById("input-hobby")
+const maleCheckBox = document.getElementById("male")
+const femaleCheckBox = document.getElementById("female")
+const otherCheckBox = document.getElementById("other")
+const avatarInput = document.getElementById("UploadAvatar")
+const jumbotron = document.getElementById("error-container")
+const errorList = document.getElementById("errors-list")
+const cancelBtn = document.getElementById("cancel-btn")
+const addHobby = document.getElementById("addhobby-btn")
+const genderOptionsWrapper = document.getElementById("genderOptionsWrapper")
+const nameAgeWrapper = document.getElementById("nameAgeWrapper")
+
+validateInput = () => {
+  messagesObj.messages.forEach(message => {
+    const liElement = document.createElement("li");
+    liElement.setAttribute("class", "li")
+    liElement.textContent = message;
+    errorList.appendChild(liElement)
+    jumbotron.style.display = "block"
+
+  })
+  // nameInput.value = "";
+  // ageInput.value = "";
+  const hobbiesCount = document.getElementById("hobbies-num")
+  hobbiesCount.textContent = (`Your hobbies`);
+  localStorage.clear()
+}
+
+avatarInput.onchange = (e) => {
+
+  const reader = new FileReader()
+
+  reader.onload = () => {
+    localStorage.setItem("avatar", reader.result)
+  }
+  reader.readAsDataURL(avatarInput.files[0])
+}
+let hobbies = []
+addHobby.onclick = (e) => {
+
+  e.preventDefault()
+  if (!hobbyInput.value) {
+    messagesObj.messages.push("Please insert a hobby to add");
+    validateInput()
+  }
+  hobbies.push(hobbyInput.value)
+  hobbyInput.value = ""
+  const hobbiesNumber = document.getElementById("hobbies-num")
+
+  hobbiesNumber.textContent = (`You have ${hobbies.length} hobbies`)
+
+}
+
+cancelBtn.onclick = (e) => {
+
+  e.preventDefault()
+
+  location.href = "file:///C:/Users/Mahmoud/Desktop/create-profile/main.html"
+}
+
+let messagesObj = {
+  ok: true,
+  messages: []
+}
+
+btn.onclick = function (e) {
+  e.preventDefault()
+  const ul = document.getElementById("errors-list");
+  while (ul.hasChildNodes()) {
+    ul.removeChild(ul.firstChild);
+  }
+
+  messagesObj.messages = []
+  if (nameInput.value.length > 20 || nameInput.value.length == 0) {
+    messagesObj.ok = false
+    messagesObj.messages.push("Please enter name up to 20 character")
+
+  }
+  if (ageInput.value >= 150 || ageInput.value == 0) {
+    messagesObj.ok = false
+    messagesObj.messages.push("Age required, Max 150")
+  }
+  if (maleCheckBox.checked === false && femaleCheckBox.checked === false && otherCheckBox.checked === false) {
+    messagesObj.ok = false
+    messagesObj.messages.push("Please select gender")
+  }
+
+  if (messagesObj.ok == false) {
+    validateInput()
+
+  }
+  else {
+    localStorage.setItem("name", nameInput.value)
+    localStorage.setItem("age", ageInput.value)
+    localStorage.setItem("hobbies", hobbies)
+    nameInput.value = "";
+    ageInput.value = "";
+    if (femaleCheckBox.checked === true) {
+      localStorage.removeItem("male");
+      localStorage.removeItem("other")
+      localStorage.setItem("female", femaleCheckBox.value)
+    } if (maleCheckBox.checked === true) {
+      localStorage.removeItem("female");
+      localStorage.removeItem("other")
+      localStorage.setItem("male", maleCheckBox.value)
+    } if (otherCheckBox.checked === true) {
+      localStorage.removeItem("male");
+      localStorage.removeItem("female")
+      localStorage.setItem("other", otherCheckBox.value)
+    }
+
+    location.href = "file:///C:/Users/Mahmoud/Desktop/create-profile/profile.html";
+  }
+}
+nameAgeWrapper.onchange = (e) => {
+  e.preventDefault()
+  messagesObj.ok = true;
+  messagesObj.messages = []
+  jumbotron.style.display = "none"
+}
+
+genderOptionsWrapper.onchange = (e) => {
+  e.preventDefault()
+  messagesObj.ok = true;
+  messagesObj.messages = []
+  jumbotron.style.display = "none"
+}
---
+++ profile.js
@@ -0,0 +1,81 @@
+const getContainer = document.getElementById("details-container")
+
+const imageDiv = createAppendElement("div", getContainer, { id: "imageDiv" });
+const img = createAppendElement("img", imageDiv, { id: "image", alt: "profile", width: "200px", height: "200px" })
+// const img = new Image()
+// img.id = "image";
+// img.alt = "profile"
+// img.width = 200;
+// img.height = 200;
+
+imageDiv.appendChild(img);
+if (localStorage.getItem("avatar")) {
+  img.setAttribute("src", localStorage.getItem("avatar"));
+} else {
+  img.src = "https://upload.wikimedia.org/wikipedia/commons/1/1e/Default-avatar.jpg"
+}
+
+
+const nameDiv = createAppendElement("div", getContainer, { id: "nameDiv" })
+const nameValue = localStorage.getItem("name")
+nameDiv.textContent = `Name: ${nameValue}`
+
+
+
+
+const ageDiv = createAppendElement("div", getContainer, { id: "ageDiv" })
+const ageValue = localStorage.getItem("age")
+ageDiv.textContent = `Age: ${ageValue}`;
+
+const maleValue = localStorage.getItem("male")
+const femaleValue = localStorage.getItem("female")
+const otherValue = localStorage.getItem("other")
+
+if (maleValue) {
+  const maleDiv = createAppendElement("div", getContainer, { class: "genderOpt" })
+  maleDiv.textContent = `Gender: ${maleValue}`
+} else if (femaleValue) {
+  const femaleDiv = createAppendElement("div", getContainer, { class: "genderOpt" })
+  femaleDiv.textContent = `Gender: ${femaleValue}`
+} else if (otherValue) {
+  const otherDiv = createAppendElement("div", getContainer, { class: "genderOpt" })
+  otherDiv.textContent = `Gender: ${otherValue}`
+}
+
+let hobbiesValue = localStorage.getItem("hobbies");
+console.log(hobbiesValue.length);
+const hobbiesList = createAppendElement("ul", getContainer, { id: "hobbiesList" })
+hobbiesList.innerHTML = "Hobbies"
+if (!hobbiesValue.length) {
+  const hobbyItem = createAppendElement("li", hobbiesList, { class: "hobbyItem" })
+  hobbyItem.textContent = `No hobbies entered`;
+} else {
+  hobbiesValue = hobbiesValue.split(",")
+  hobbiesValue.forEach(hobby => {
+
+    const hobbyItem = createAppendElement("li", hobbiesList, { class: "hobbyItem" })
+    hobbyItem.textContent = hobby;
+  })
+}
+
+const backBtn = createAppendElement("button", getContainer, { id: "backBtn", class: "btn btn-success" })
+backBtn.textContent = "Start Over";
+backBtn.onclick = () => {
+  localStorage.clear()
+  location.href = "file:///C:/Users/Mahmoud/Desktop/create-profile/createuser.html"
+}
+
+function createAppendElement(name, parent, options = {}) {
+  const htmlElem = document.createElement(name);
+  parent.appendChild(htmlElem);
+  Object.keys(options).forEach(key => {
+    const value = options[key];
+    if (key === 'html') {
+      htmlElem.innerHTML = value;
+    } else {
+      htmlElem.setAttribute(key, value);
+    }
+  });
+
+  return htmlElem;
+}
